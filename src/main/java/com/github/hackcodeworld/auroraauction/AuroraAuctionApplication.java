0d57c4217519b59a30f5dd5b03243e748f41f130
amg <ballerbrabus@outlook.com>
2024-02-24 03:28:51 -0600

    init project

---
+++ src/main/java/com/github/hackcodeworld/auroraauction/AuroraAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.github.hackcodeworld.auroraauction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuroraAuctionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuroraAuctionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/hackcodeworld/auroraauction/AuroraAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hackcodeworld.auroraauction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuroraAuctionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
